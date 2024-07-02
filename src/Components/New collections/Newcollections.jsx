a26ca44e7020139670a4059279d360e77d9e490e
Ayush  singh Chauhan <130600276+ayush121314@users.noreply.github.com>
2024-07-02 22:03:18 +0530

    Add files via upload

--- src/Components/New collections/Newcollections.jsx
+++ src/Components/New collections/Newcollections.jsx
@@ -1,25 +1,37 @@
-import React, { useEffect, useState  } from 'react'
-import { Items } from '../Item/Items'
-export const Newcollections = () => { 
+import React, { useEffect, useState } from 'react';
+import { Items } from '../Item/Items';
+import LoadingSpinner from '../LoadingSpinner/LoadingSpinner'; 
+
+const Newcollections = () => {
   const [New_collections, setNew_collections] = useState([]);
-  useEffect(()=>{
-       fetch('https://ecom-back-yale.onrender.com/newcollections')
-       .then((res)=>res.json())
-       .then((data)=>setNew_collections(data))
-  },[])
-  
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('https://ecom-back-yale.onrender.com/newcollections')
+      .then((res) => res.json())
+      .then((data) => {
+        setNew_collections(data);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <LoadingSpinner />;
+  }
+
   return (
-    <div className='w-[100%]  flex flex-col'>
-    <div className='text-[50px] text-center font-[600]'>New collections</div>
-    <hr className='w-[200px] h-[6px] self-center rounded-[10px] bg-[#252525]'></hr>
-        <div className='popular-item flex flex-wrap  mt-[50px] lg:mx-[100px] md:mx-[70px ] mb-[10px] gap-[30px] justify-center'>
+    <div className='w-[100%] flex flex-col'>
+      <div className='text-[50px] text-center font-[600]'>New collections</div>
+      <hr className='w-[200px] h-[6px] self-center rounded-[10px] bg-[#252525]'></hr>
+      <div className='popular-item flex flex-wrap mt-[50px] lg:mx-[100px] md:mx-[70px] mb-[10px] gap-[30px] justify-center'>
         {
-            New_collections.map((item,i)=>{
-            return <Items key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
-        })
+          New_collections.map((item, i) => {
+            return <Items key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
+          })
         }
+      </div>
     </div>
-    </div>
-  )
-}
-export default Newcollections
+  );
+};
+
+export default Newcollections;
--- src/Components/Popular/Popular.jsx
+++ src/Components/Popular/Popular.jsx
@@ -1,23 +1,35 @@
-import React, { useEffect, useState } from 'react'
-import { Items } from '../Item/Items'
+import React, { useEffect, useState } from 'react';
+import { Items } from '../Item/Items';
+import LoadingSpinner from '../LoadingSpinner/LoadingSpinner'; 
 
-
-export const Popular = () => {
+const Popular = () => {
   const [data_product, setpopwom] = useState([]);
-  useEffect(()=>{
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
     fetch('https://ecom-back-yale.onrender.com/popularwom')
-    .then((res)=>res.json())
-    .then((data)=>setpopwom(data))
-  },[])
+      .then((res) => res.json())
+      .then((data) => {
+        setpopwom(data);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <LoadingSpinner />;
+  }
+
   return (
     <div className='popular flex flex-col gap-[10px]'>
-    <h1 className='text-[50px] font-[600] text-[#171717] text-center'>Popular in women</h1>
-    <hr className='w-[200px] h-[6px] rounded-[10px] bg-[#252525] self-center'></hr>
-    <div className='popular-item flex flex-wrap justify-center mt-[50px] lg:mx-[100px] md:mx-[70px] mb-[10px] gap-[30px]'>
-        {data_product.map((item,i)=>{
-            return <Items key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
+      <h1 className='text-[50px] font-[600] text-[#171717] text-center'>Popular in women</h1>
+      <hr className='w-[200px] h-[6px] rounded-[10px] bg-[#252525] self-center'></hr>
+      <div className='popular-item flex flex-wrap justify-center mt-[50px] lg:mx-[100px] md:mx-[70px] mb-[10px] gap-[30px]'>
+        {data_product.map((item, i) => {
+          return <Items key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
         })}
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
+
+export default Popular;
