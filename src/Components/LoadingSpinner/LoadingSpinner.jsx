bbaec33536530e6ef64fb90df77b1f011b309e9f
Ayush  singh Chauhan <130600276+ayush121314@users.noreply.github.com>
2024-07-03 16:58:50 +0530

    Update LoadingSpinner.jsx

--- src/Components/LoadingSpinner/LoadingSpinner.jsx
+++ src/Components/LoadingSpinner/LoadingSpinner.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const LoadingSpinner = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen">
+    <div className="flex flex-col items-center justify-center min-h-screen">
       <div className="animate-spin rounded-full h-32 w-32 border-t-4 border-blue-500"></div>
       <div className='text-xl'>Loading products....</div>
     </div>
