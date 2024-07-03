44646abbdd7e0a0a2fcf4ddefc2c53bec0a7e555
Ayush  singh Chauhan <130600276+ayush121314@users.noreply.github.com>
2024-07-03 16:32:19 +0530

    Update LoadingSpinner.jsx

--- src/Components/LoadingSpinner/LoadingSpinner.jsx
+++ src/Components/LoadingSpinner/LoadingSpinner.jsx
@@ -4,7 +4,7 @@ const LoadingSpinner = () => {
   return (
     <div className="flex items-center justify-center min-h-screen">
       <div className="animate-spin rounded-full h-32 w-32 border-t-4 border-blue-500"></div>
-{/*       <div className='text-xl'>Please wait we are<br>Loading products....</br></div> */}
+      <div className='text-xl>Loading products....</div>
     </div>
   );//d
 };
